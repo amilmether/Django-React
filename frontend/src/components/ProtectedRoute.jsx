31a6ace199d27b172cf1451244e3479f39962965
Amil <amilmether37@gmail.com>
2025-03-02 17:29:27 +0530

    edited spelling mistake protectedRoutes

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -2,16 +2,16 @@ import {Navigate} from "react-router-dom"
 import {jwtDecode} from "jwt-decode"
 import api from "../api"
 import { REFRESH_TOKEN , ACCESS_TOKEN } from "../contants"
-import { userState,useEffect} from "react"
+import { useState,useEffect} from "react"
 
 function ProtectedRoute({children}){
-    const [isAuthorized,setIsAuthorized] = userState(null)
+    const [isAuthorized,setIsAuthorized] = useState(null);
 
     useEffect(()=>{
         auth().catch(() => setIsAuthorized(false))
     },[])
     const refreshToken = async () =>{
-        const refreshToken = localStorage.getItem(REFRESH_TOKEN)
+        const refreshToken = localStorage.getItem(REFRESH_TOKEN);
         try{
             const res =await api.post("/api/token/refresh/",{
                 refresh : refreshToken,
@@ -23,26 +23,26 @@ function ProtectedRoute({children}){
                 setIsAuthorized(false)
             }
         }catch(error){
-            console.log(error)
-            setIsAuthorized(false)
+            console.log(error);
+            setIsAuthorized(false);
         }
-    }
+    };
 
     const auth = async () =>{
-        const token = localStorage.getItem(ACCESS_TOKEN)
+        const token = localStorage.getItem(ACCESS_TOKEN);
         if (!token){
-            setIsAuthorized(false)
-            return
+            setIsAuthorized(false);
+            return;
         }
-        const decode = jwtDecode(token)
-        const tokenExpiration = Decode.exp
-        const now = Date.now()/1000
+        const decode = jwtDecode(token);
+        const tokenExpiration = Decode.exp;
+        const now = Date.now()/1000;
         if(tokenExpiration < now){
-            await refreshToken()
+            await refreshToken();
         }else{
-            setIsAuthorized(true)
+            setIsAuthorized(true);
         }
-    }
+    };
 
     if (isAuthorized === null){
         return <div>Loading...</div>
